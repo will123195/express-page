00b347fdfcf10996d2da6cc58bcb5d63f5029afa
will123195 <will123195@gmail.com>
2014-11-01 18:00:22 -0400

    set()

--- index.js
+++ index.js
@@ -19,28 +19,41 @@ var Page = module.exports = function(controller, helpers, req, res) {
   self._layout = null
 
   helpers = helpers || {}
-  Object.keys(helpers).forEach(function(helper) {
-    self[helper] = helpers[helper]
-  })
+  self.set(helpers)
+
+  return self
 }
 
 Page.prototype.run = function() {
   this._controller(this)
+  return this
+}
+
+Page.prototype.set = function(helpers) {
+  var self = this
+  Object.keys(helpers).forEach(function(helper) {
+    self[helper] = helpers[helper]
+  })
+  return self
 }
 
 Page.prototype.notFound = function() {
-  this.res.status(404).send('Not found.');
+  this.res.status(404).send('Not found.')
+  return this
 }
 
 Page.prototype.accessDenied = function() {
   this.res.status(403).send('Access denied.')
+  return this
 }
 
 Page.prototype.redirect = function() {
   this.res.redirect.apply(this, arguments)
+  return this
 }
 
 Page.prototype.render = function() {
+  var self = this
   if (!this.templates[this._view]) {
     var msg = 'No template for ' + this._view
     throw new Error(msg)
@@ -51,12 +64,16 @@ Page.prototype.render = function() {
     html = this.templates[this._layout](this)
   }
   this.res.send(html)
+  return self
 }
 
 Page.prototype.setLayout = function(template) {
   this._layout = template
+  return this
 }
 
 Page.prototype.setView = function(template) {
   this._view = template
+  return this
 }
+
